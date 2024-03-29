85b833a324f080ef82118f7deec344de2dab9003
Michael Schmidt <michael.schmidt@wuerth-it.com>
2024-03-29 17:09:37 +0100

    Using graphql to get data

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,89 @@
 <script lang="ts">
+  import { ApolloClient, InMemoryCache, ApolloProvider, gql, HttpLink, split } from '@apollo/client';
+  import { getMainDefinition } from '@apollo/client/utilities';
+  import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+  import { createClient } from 'graphql-ws';
   import Heatmap from "./lib/Heatmap.svelte"
-  let temps = [{
+  import { onMount } from 'svelte'
+
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:3000/graphql'
+  });
+
+
+  const wsLink = new GraphQLWsLink(createClient({
+    url: 'ws://localhost:3000/graphql',
+  }));
+
+  const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
+  );
+
+  const client = new ApolloClient({  
+    link: splitLink,
+    cache: new InMemoryCache(),
+  });
+
+  let temps : {
+    place: string,
+    temperature: number,
+    timestamp: Date
+  }[] = []
+
+  onMount(async () => {
+
+    const res = await client
+      .query({
+        query: gql`
+          query {
+            currentTemperatureData {
+              seat
+              value
+              timestamp
+            }
+          }
+        `,
+      })
+    temps = res.data.currentTemperatureData.map((t: {seat: string, value: number, timestamp: Date}) => {
+      return {
+        place: t.seat,
+        temperature: t.value,
+        timestamp: t.timestamp
+      }
+    })
+    console.log(res)
+
+    const subscription = await client.subscribe({query: gql`
+      subscription {
+        seatDataChanged {
+          seat
+          value
+          timestamp
+        }
+      }
+    `})
+
+    subscription.subscribe((update) => {
+      const updatedData = update.data.seatDataChanged
+      const index = temps.findIndex((t) => t.place === updatedData.seat)
+      temps[index] = {
+        place: updatedData.seat,
+        temperature: updatedData.value,
+        timestamp: updatedData.timestamp
+      }
+    })
+
+  })
+
+  /*let temps = [{
     place: "A1",
     temperature: 14,
     timestamp: new Date()
@@ -12,9 +95,9 @@
     place: "A20",
     temperature: 34,
     timestamp: new Date()
-  }]
+  }]*/
 
-  setTimeout(() => {
+  /*setTimeout(() => {
     console.log('Updating positions')
     temps = [{
       place: "B1",
@@ -29,7 +112,8 @@
       temperature: 34,
       timestamp: new Date()
     }]
-  }, 10000)
+  }, 10000)*/
+
 </script>
   
 
